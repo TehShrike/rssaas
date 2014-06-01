73627de39340efce2e956fe2a28ff054fef6e7b7
Josh Duff <me@JoshDuff.com>
2014-06-01 03:20:50 +0000

    Now using noddity-service-server

--- index.js
+++ index.js
@@ -1,105 +1,72 @@
 var url = require('url')
-var qs = require('querystring')
-var StringMap = require('stringmap')
-var Butler = require('noddity-butler')
 var Rss = require('rss')
-var level = require('level')
-var sanitize = require("sanitize-filename")
-var safeConverter = require('pagedown').getSanitizingConverter()
-var markdownToHtml = safeConverter.makeHtml.bind(safeConverter)
-var joinPath = require('path').join
-var Linkifier = require('noddity-linkifier')
+var getPostHtml = require('noddity-renderer').makeHtmlFromPost
+var async = require('async')
 
-function dumbResolve(firstThingy, secondThingy) {
-	var separator = '/'
-	if (firstThingy[firstThingy.length - 1] === '/') {
-		separator = ''
-	}
-	return firstThingy + separator + secondThingy
-}
+module.exports = function getRssFeedXml(context, cb) {
+	if (typeof context.parameters.title === 'undefined') {
+		cb(new Error("'title' must be provided"))
+	} else if (typeof context.parameters.author === 'undefined') {
+		cb(new Error("'author' must be provided"))
+	} else {
+		var butler = context.butler
+		var postUrlRoot = context.postUrlRoot
+		var dumbResolve = context.resolvePost
+		var blogTitle = context.parameters.title
+		var blogAuthor = context.parameters.author
+		var feedUrl = context.url
+		var linkify = context.linkify
 
-function getRssFeedXml(butler, feedUrl, postUrlRoot, blogTitle, blogAuthor, cb) {
-	butler.getPosts({ mostRecent: 7 }, function(err, posts) {
-		if (err) {
-			cb(err)
-		} else {
-			var siteRootUrl = url.resolve(postUrlRoot, '')
-			var rss = new Rss({
-				title: blogTitle || siteRootUrl,
-				feed_url: feedUrl,
-				site_url: siteRootUrl,
-				ttl: 12 * 60
-			})
-
-			var addToFeed = rss.item.bind(rss)
-
-			posts.reverse()
-
-			var linkify = new Linkifier(postUrlRoot)
-
-			posts.map(function turnPostIntoRssItem(post) {
-				var postUrl = dumbResolve(postUrlRoot, post.filename)
-				return {
-					title: post.metadata.title || post.filename,
-					description: linkify(markdownToHtml(post.content)),
-					url: postUrl,
-					// Post URLs must be unique!
-					// guid: '',
-					author: post.metadata.author || blogAuthor,
-					date: post.metadata.date
-				}
-			}).forEach(addToFeed)
-			cb(err, rss.xml())
-		}
-	})
-}
-
-function allNecessaryParametersExist(parameters) {
-	return typeof parameters.noddityRoot !== 'undefined'
-		&& typeof parameters.postUrlRoot !== 'undefined'
-		&& typeof parameters.title !== 'undefined'
-		&& typeof parameters.author !== 'undefined'
-}
-
-module.exports = function Rssaas() {
-
-	var butlers = new StringMap()
-
-	function getAppropriateButler(rootUrl) {
-		if (!butlers.has(rootUrl)) {
-			var db = level(joinPath('/tmp', sanitize(rootUrl)))
-			var butler = new Butler(rootUrl, db)
-			butlers.set(rootUrl, butler)
+		function turnPostIntoRssItem(post) {
+			var postUrl = dumbResolve(post.filename)
+			return {
+				title: post.metadata.title || post.filename,
+				description: post.html,
+				url: postUrl,
+				// Post URLs must be unique!
+				// guid: '',
+				author: post.metadata.author || blogAuthor,
+				date: post.metadata.date
+			}
 		}
 
-		return butlers.get(rootUrl)
-	}
+		butler.getPosts({ mostRecent: 7 }, function(err, posts) {
+			if (err) {
+				cb(err)
+			} else {
+				var siteRootUrl = url.resolve(postUrlRoot, '')
+				var rss = new Rss({
+					title: blogTitle,
+					feed_url: feedUrl,
+					site_url: siteRootUrl,
+					ttl: 12 * 60
+				})
 
-	var server = require('http').createServer(function(req, res) {
-		var parameters = qs.parse(url.parse(req.url).query)
-		if (allNecessaryParametersExist(parameters)) {
-			var butler = getAppropriateButler(parameters.noddityRoot)
+				var addToFeed = rss.item.bind(rss)
 
-			getRssFeedXml(butler,
-					req.url,
-					parameters.postUrlRoot,
-					parameters.title,
-					parameters.author,
-					function (err, xml) { res.end(err || xml) })
-		} else {
-			res.end("Need to supply noddityRoot, postUrlRoot, and title")
-		}
-	})
+				posts.reverse()
 
-	var closeServer = server.close.bind(server)
-
-	server.close = function closeRssaasServer() {
-		Object.keys(butlers.obj).forEach(function(key) {
-			var butler = butlers.get(key)
-			butler.stop()
+				async.map(posts, function(post, cb) {
+					getPostHtml(butler.getPost, linkify, post, {
+						pathPrefix: context.parameters.postUrlRoot,
+						title: post.metadata.title
+					}, function(err, html) {
+						if (!err) {
+							post.html = html
+							cb(null, post)
+						} else {
+							cb(err)
+						}
+					})
+				}, function(err, posts) {
+					if (err) {
+						cb(err)
+					} else {
+						posts.map(turnPostIntoRssItem).forEach(addToFeed)
+						cb(null, rss.xml())
+					}
+				})
+			}
 		})
-		closeServer()
 	}
-
-	return server
 }
